bbc2772451e3e3d07395816d3057b6d909a1e53b
John Kotz <john.lyme@mac.com>
2016-10-31 20:36:32 -0400

    Pushit push

--- cloud/main.js
+++ cloud/main.js
@@ -65,7 +65,7 @@ Parse.Cloud.define('onSignUp', function(request, response) {
 	// This function will take the survey results, store them in the given user, and then determine league and profile picture
 
 	var userId = request.user;
-	console.log("User id: " + JSON.stringify(userId));
+	console.log("User id: " + userId.objectId);
 	// We don't know yet what we are gonna ask, so no survey for now
 	var surveyInfo = request["survey"];
 
